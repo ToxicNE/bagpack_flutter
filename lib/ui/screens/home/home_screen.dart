92c9e049c83a446304acc55249088989dbf2d2af
Max-IThub <Nikolaev.max05@gmail.com>
2024-03-05 02:05:31 +0300

    next commit

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -8,15 +8,63 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+ void showAlertDialogAddProduct() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder:(context) {
+        return AlertDialog(
+          title: const Text('Ты хочешь добавить продукт?'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: Navigator.of(context).pop,
+             child: Text('Хочу!', style: TextStyle(color: Colors.deepPurple.shade400, fontSize: 25),)
+             ),
+          ],
+        );
+      },
+    );
+    
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
      appBar: AppBar(
-       title: const Text("Home Screen"),
-       centerTitle: true,
-       elevation: 0,
-       backgroundColor: Colors.transparent,
+    title: const Text("Главная"),
+    centerTitle: true,
+    elevation: 0,
+    backgroundColor: Colors.deepPurple.shade400,
+    foregroundColor: Colors.black,
+    leading: IconButton(
+      onPressed: () {},
+      icon: const Icon(Icons.menu),
+      color: Colors.black,
+    ),
+   ),
+
+     body: const Center(
+       child: SingleChildScrollView(
+        child: Text("Содержимое экрана", style: TextStyle(fontSize: 25),) ,
+       ),
+     ),
+     floatingActionButton: FloatingActionButton(
+       onPressed: () {
+        //тут должна быть логика добавления продукта на выбор, либо из памяти, либо сфоткать сейчас
+        return showAlertDialogAddProduct();
+       },
+       backgroundColor: Colors.deepPurple.shade400,
        foregroundColor: Colors.black,
+       child: const Icon(Icons.add),
      ),
     );
   }
