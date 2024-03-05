0d34fc3ea1187b39c416a006cf61f3575377503c
Max-IThub <Nikolaev.max05@gmail.com>
2024-03-05 03:07:29 +0300

    добавил drawer

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,16 @@ import 'package:bagpack/ui/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const MaterialApp(home: App()));
 }
 
 class App extends StatelessWidget {
   const App({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(home: HomeScreen());
   }
 }
+
+
--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -8,64 +8,121 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String selectedPage = '';
 
- void showAlertDialogAddProduct() async {
+  void showAlertDialogAddProduct() async {
     return showDialog<void>(
       context: context,
       barrierDismissible: false,
-      builder:(context) {
+      builder: (context) {
         return AlertDialog(
           title: const Text('Ты хочешь добавить продукт?'),
           content: const SingleChildScrollView(
             child: ListBody(
-              children: <Widget>[
-                
-              ],
+              children: <Widget>[],
             ),
           ),
           actions: <Widget>[
             TextButton(
-              onPressed: Navigator.of(context).pop,
-             child: Text('Хочу!', style: TextStyle(color: Colors.deepPurple.shade400, fontSize: 25),)
-             ),
+                onPressed: Navigator.of(context).pop,
+                child: Text(
+                  'Хочу!',
+                  style: TextStyle(
+                      color: Colors.deepPurple.shade400, fontSize: 25),
+                )),
           ],
         );
       },
     );
-    
-    
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-    title: const Text("Главная"),
-    centerTitle: true,
-    elevation: 0,
-    backgroundColor: Colors.deepPurple.shade400,
-    foregroundColor: Colors.black,
-    leading: IconButton(
-      onPressed: () {},
-      icon: const Icon(Icons.menu),
-      color: Colors.black,
-    ),
-   ),
-
-     body: const Center(
-       child: SingleChildScrollView(
-        child: Text("Содержимое экрана", style: TextStyle(fontSize: 25),) ,
-       ),
-     ),
-     floatingActionButton: FloatingActionButton(
-       onPressed: () {
-        //тут должна быть логика добавления продукта на выбор, либо из памяти, либо сфоткать сейчас
-        return showAlertDialogAddProduct();
-       },
-       backgroundColor: Colors.deepPurple.shade400,
-       foregroundColor: Colors.black,
-       child: const Icon(Icons.add),
-     ),
+      appBar: AppBar(
+        title: const Text("Главная"),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.deepPurple.shade400,
+        foregroundColor: Colors.black,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+             const DrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image:NetworkImage('https://www.wmj.ru/imgs/2020/10/23/18/4300765/75ca401ba4ef59c4222c54c26ac7cc22d032b145.jpg'),
+                )
+              ),
+              child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+              padding:  EdgeInsets.only(bottom: 16.0, left: 16.0),
+              child: Text(
+                'Имя пользователя',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.message),
+              title: const Text('Сообщения', style: TextStyle(fontSize: 25)),
+              onTap: () {
+                setState(() {
+                  selectedPage = 'Сообщения';
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.account_circle),
+              title: const Text('Профиль', style: TextStyle(fontSize: 25)),
+              onTap: () {
+                setState(() {
+                  selectedPage = 'Профиль';
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Настройки', style: TextStyle(fontSize: 25)),
+              onTap: () {
+                setState(() {
+                  selectedPage = 'Настройки';
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Text(
+            ' $selectedPage',
+            style: const TextStyle(fontSize: 25),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //тут должeн  быть список добавления продуктов на выбор, либо из памяти, либо сфоткать сейчас
+          return showAlertDialogAddProduct();
+        },
+        backgroundColor: Colors.deepPurple.shade400,
+        foregroundColor: Colors.black,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
